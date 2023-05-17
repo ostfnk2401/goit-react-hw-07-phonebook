05955744c86e8a7f4b4c7ceb1de6db6015538f4e
ostfnk2401 <o.stfnk2401@gmail.com>
2023-05-17 12:28:07 +0300

    fix styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+
+import {
+  PhonebookForm,
+  SubmitBtn,
+  FormTitle,
+} from './Form.styled';
 
 const schema = Yup.object().shape({
   name: Yup.string().min(2).max(70).required('Name is required'),
-  number: Yup.number().min(4).required('Number is required'),
+  number: Yup.string().min(4).required('Number is required'),
 });
 
- export const ContactForm = ({ onSubmit }) => {
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactForm = ({ onSubmit }) => {
   const [initialValues, setInitialValues] = useState({
     name: '',
     number: '',
@@ -23,24 +35,30 @@ const schema = Yup.object().shape({
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={schema}
-    >
-      <Form autoComplete="off">
-        <label htmlFor="name">
-          <div>Name</div>
-          <Field type="text" name="name" />
-          <ErrorMessage name="name" component="div" className="error" />
-        </label>
-        <label htmlFor="number">
-          <div>Number</div>
-          <Field type="tel" name="number" />
-          <ErrorMessage name="number" component="div" className="error" />
-        </label>
-        <button type="submit">Add contact</button>
-      </Form>
-    </Formik>
+    <PhonebookForm>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+      >
+        <Form autoComplete="off">
+          <label htmlFor="name">
+            <FormTitle>Name</FormTitle>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" className="error" />
+          </label>
+          <label htmlFor="number">
+            <FormTitle>Number</FormTitle>
+            <Field type="text" name="number" />
+            <ErrorMessage name="number" component="div" className="error" />
+          </label>
+          <SubmitBtn type="submit">Add contact</SubmitBtn>
+        </Form>
+      </Formik>
+    </PhonebookForm>
   );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 };
